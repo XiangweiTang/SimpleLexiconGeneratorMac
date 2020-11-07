e9bd45db176aa0d9e01e53f1c39045c520869ca0
xiangweitang <tangxiangwei@gmail.com>
2020-11-07 04:58:10 -0800

    Add data source.

---
+++ SimpleLexiconGeneratorMac/Backend/AudioDataSource.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using AppKit;
+using System.Collections.Generic;
+
+namespace SimpleLexiconGeneratorMac
+{
+    class AudioDataSource : NSTableViewDataSource
+    {
+        public string[] AudioNameArray { get; set; }
+        public AudioDataSource()
+        {
+
+        }
+        public AudioDataSource(string[] array)
+        {
+            AudioNameArray = array;
+        }
+        public override nint GetRowCount(NSTableView tableView)
+        {
+            return AudioNameArray.Length;
+        }
+    }
+
+    class AudioViewDelegate : NSTableViewDelegate
+    {
+        private AudioDataSource DataSource;
+        public AudioViewDelegate(AudioDataSource dataSource)
+        {
+            DataSource = dataSource;
+        }
+        public override NSView GetViewForItem(NSTableView tableView, NSTableColumn tableColumn, System.nint row)
+        {
+            NSTextField view = (NSTextField)tableView.MakeView("", this);
+            if (view == null)
+            {
+                view = new NSTextField()
+                {
+                    Identifier = "",
+                    BackgroundColor = NSColor.Clear,
+                    Bordered = false,
+                    Selectable = false,
+                    Editable = false
+                };
+            }
+            view.StringValue = DataSource.AudioNameArray[(int)row];
+            return view;
+        }
+    }
+}
